228a4774b7b42daaa67f1f57eb3ebea486ee1670
akash1114 <68108181+akash1114@users.noreply.github.com>
2021-02-26 17:32:46 +0530

    Update models.py

--- devedend_data/models.py
+++ devedend_data/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-class data(models.Model):
-    compny_name = models.CharField(max_length=100,primary_key=True)
-    type = models.CharField(max_length=50)
-    rate = models.FloatField()
-    announcment = models.DateField()
-    record = models.DateField()
-    ex_devedend = models.DateField()
-    dividend_fv =  models.FloatField()
-    dividend_mp = models.FloatField()
