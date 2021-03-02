9792c83edf6f499d332e02f0cdb99d44c1f8c46e
akash <akashpalan99@gmail.com>
2021-03-02 10:01:28 +0530

    Hourly update

--- devedend_data/apps.py
+++ devedend_data/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class StockDataConfig(AppConfig):
     name = 'devedend_data'
+    def ready(self):
+        from devedend_data.sheculer import updater
+        updater.start()
